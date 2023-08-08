07f2bf0aed9286631411fbcbaa6033ab3cc302ec
dineshWic <109039671+dineshWic@users.noreply.github.com>
2023-08-08 08:51:30 +0530

    create login, registration and navbars in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
+import { useMemo } from "react";
 import { BrowserRouter,Navigate, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+
+
 import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage/indx";
+import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
 
   return (
-
     <div className="app">
       <BrowserRouter>
-        <Routes>
-           <Route path="/" element={<LoginPage />} />
-           <Route path="/home" element={<HomePage />} />
-           <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={ isAuth? <HomePage />:<Navigate to = "/" />} />
+            <Route path="/profile/:userId" element={ isAuth? <ProfilePage /> :<Navigate to = "/" /> } />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
