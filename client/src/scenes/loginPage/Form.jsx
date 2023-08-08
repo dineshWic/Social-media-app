be51902a856e651a4ba62a63e9de4eec16c6d11d
dineshWic <109039671+dineshWic@users.noreply.github.com>
2023-08-08 16:55:43 +0530

    create pages and add fetch logic and add images

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
-import * as yup from "yup"; //for validation
+import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setLogin } from "state";
